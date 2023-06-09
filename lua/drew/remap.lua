d79e32df4ce0b3a12dcd14348a48725f83f0b6a8
d4n13ls1104 <d4n13lsofficial@gmail.com>
2023-06-09 03:15:45 -0400

    disable mouse

--- lua/drew/remap.lua
+++ lua/drew/remap.lua
@@ -2,3 +2,8 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jk", "<Esc>", {})
 
+vim.opt.mouse = 'nvic'
+
+vim.api.nvim_set_keymap('n', '<LeftDrag>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<LeftRelease>', '<Nop>', { noremap = true, silent = true })
+
